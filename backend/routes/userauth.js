99ff4b19b40e29d33aef3a2832ccd68cf3d964a9
sarthak mehta <sarthakmehta768@gmail.com>
2024-12-25 10:53:41 +0530

    sarthak's forth commit

--- backend/routes/userauth.js
+++ backend/routes/userauth.js
@@ -3,12 +3,14 @@ const router=express.Router();
 const userauth=require('../controllers/userauth');
 const adminauth=require('../controllers/adminauth');
 const Aq=require('../controllers/Aq');
-const questionpage=require('../controllers/usertest')
+const questionpage=require('../controllers/usertest');
+const score=require('../controllers/pdf')
 
 router.route("/user/auth").post(userauth);
 router.route("/admin/auth").post(adminauth)
 router.route("/question/uploading/page").post(Aq);
 router.route("/test/page").post(questionpage);
+router.route("/score/page").post(score);
 
 
 module.exports=router
