bcc8d64c5f06e570f7cf903e89c720edbceab19a
sarhak234 <168266094+sarhak234@users.noreply.github.com>
2025-03-21 14:18:01 +0530

    Update vite.config.js

--- frontend/vite-project/vite.config.js
+++ frontend/vite-project/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    historyApiFallback: true
+  }
+});
+
